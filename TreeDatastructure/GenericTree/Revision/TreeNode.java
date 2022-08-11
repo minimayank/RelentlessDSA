61471f894e035d8ea3e65a54baba79e03bd9e1eb
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-08-11 14:35:59 +0530

    Created a TreeNode

---
+++ TreeDatastructure/GenericTree/Revision/TreeNode.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class TreeNode<T> {
+    T data;
+    ArrayList<TreeNode<T>>children;
+   TreeNode(T data){
+       this.data=data;
+       children=new ArrayList<>();
+    }
+}
