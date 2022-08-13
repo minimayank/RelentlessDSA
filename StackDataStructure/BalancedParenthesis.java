a103a6b087a5dd399667c8a7505796586a314f02
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-08-13 21:28:48 +0530

    Revision of the day,,,,

--- StackDataStructure/BalancedParenthesis.java
+++ StackDataStructure/BalancedParenthesis.java
@@ -1,30 +1,27 @@
-package Revision;
-
 import java.util.ArrayDeque;
 import java.util.Deque;
-
+import java.util.Scanner;
 public class BalancedParenthesis {
-    public static boolean matched(Character a, Character b){
-        return ( a=='('&& b==')'||
-                (a=='{'&& b=='}')||
-                (a=='['&& b==']'));
+    public static boolean matching(Character a,Character b){
+        return (a=='('&&b==')'||
+                a=='{'&&b=='}'||
+                a=='['&&b==']');
     }
-    public static boolean balanacedParenthesisCheck(String input){
-        if(input.length()==0)return true;
+    public static boolean isBalanced(String expression){
+        if(expression.length()%2!=0)return false;
+        if(expression.length()==0)return true;
         else{
-            Deque<Character> stack=new  ArrayDeque<>();
-            for(int i=0;i<input.length();i++){
-                Character x=input.charAt(i);
-                if(x=='['||x=='{'||x=='('){
-                    stack.push(x);
+            Deque<Character>stack=new ArrayDeque<>();
+            for(int i=0;i<expression.length();i++){
+                Character c=expression.charAt(i);
+                if (c == '(' || c == '{' || c =='[') {
+                    stack.push(c);
                 }
-
                 else {
                     if (stack.isEmpty()) return false;
-                    if (matched(stack.peek(), x) == false) {
-                        return false;
-                    } else {
-                        stack.pop();
+                    if (!matching(stack.peek(), c)) return false;
+                    else {
+                         stack.pop();
                     }
                 }
             }
@@ -32,7 +29,20 @@ public class BalancedParenthesis {
         }
     }
     public static void main(String[] args) {
-        String input="((({}))";
-        System.out.println(balanacedParenthesisCheck(input));
+        System.out.println("Balanced Parenthesis Problem ");
+        System.out.println("Enter the number of testcases: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       for(int i=0;i<n;i++){
+           System.out.println("Enter the expression....of parenthesis (){}[] only.. ");
+           String expression=sc.next();
+           String ans="is Balanced.";
+           if(!isBalanced(expression)){
+               ans="is not Balanced.";
+           }
+           System.out.println("Expression "+expression+" "+ans);
+       }
+        System.out.println("Thank You!!");
+       sc.close();
     }
 }
--- StackDataStructure/CheckRedundancy.java
+++ StackDataStructure/CheckRedundancy.java
@@ -1,41 +1,56 @@
-import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class CheckRedundancy {
-    public static boolean checkRedundancy(String expression){
-        Deque<Character>stack=new ArrayDeque<>();
+    public static boolean operators(Character c){
+        return (c=='*'||c=='-'||c=='+'||c=='/');
+    }
+    public static boolean isRedundant(String expression){
+         boolean ans=false;
         if(expression.length()==0)return false;
-        else{
+        Deque<Character>stack=new ArrayDeque<Character>();
+       
+        for(int i=0;i<expression.length();i++) {
+            if (expression.charAt(i) == '(' || operators(expression.charAt(i))) {
+                stack.push(expression.charAt(i));
 
-
-            for(int i=0;i<expression.length();i++){
-                 Character c=expression.charAt(i);
-                 if(c=='(')stack.push(c);
-                 else if(c=='*'||c=='-'||c=='+'||c=='/'){
-                     stack.push(c);
-
-                 }
-                 else if(c==')') {
-                    if(stack.peek()=='(')return true;
-                     while (stack.peek()=='*'||stack.peek()=='+'||stack.peek()=='-'||stack.peek()=='/'){
-                         stack.pop();
-
-                     }
-                     stack.pop();
-                 }
             }
-            return  false;
+                if (stack.peek() == '(' && expression.charAt(i) == ')') ans= true;
+                else {
+                    if(expression.charAt(i)==')') {
+                        while (stack.peek() != '(') {
+                            stack.pop();
+                        }
+
+                        stack.pop();
+                    }
+                }
+            }
+        
+           return (ans);
         }
 
-
-
-    }
     public static void main(String[] args) {
+        System.out.println("Test for expression redundancy.. in O(n)"+
+                "\n Redundancy in an expression means that there is an unnecessary usage of brackets in a expression."+
+                "\n eg. (a+b)// here there is no redundancy."+
+                "\n eg. ((A+B))// here there is a redundancy as the second bracket has no use."+
+                "\n eg. (a)+(a+b*c)// here again there is redundancy as (a) is having unnecessary bracket. "+
+                "\n thumb rule is to check whether there an operation (*,+,-,/) or not inside the bracket");
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the expression. ");
-        String expression=sc.next();
-        sc.close();
-        System.out.println(checkRedundancy(expression));
+        System.out.println("Enter the number of test cases.");
+        int t=sc.nextInt();
+       for(int i=0;i<t;i++){
+           String expression=sc.next();
+           if(isRedundant(expression)){
+               System.out.println("Expression: "+expression+" is Redundant.");
+           }
+           else{
+               System.out.println("Expression; "+expression+" is not Redundant.");
+           }
+
+
+       }
     }
 }
