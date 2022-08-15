312df04fa341b495dd5584701bdaf8d6e1f937c3
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-08-15 12:54:43 +0530

    Create DeleteAlternateNode.java

---
+++ SinglyLinkedList/DeleteAlternateNode.java
@@ -0,0 +1,36 @@
+/* 
+
+    Following is the class used to represent the Node of a Singly Linked List
+
+    class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data) {
+            this.setData(data);
+            this.next = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+    }
+*/
+
+public class DeleteAlternateNode {
+
+    public static void deleteAlternateNodes(Node<Integer> head) {
+        //Your code goes here
+        Node<Integer>temp=head;
+        while(temp!=null&&temp.next!=null){
+            temp.next=temp.next.next;
+            temp=temp.next;
+
+        }
+    }
+}
