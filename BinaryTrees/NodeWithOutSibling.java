e80d51314a9928f677f2e2ed39e7939930ff6bab
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-08-19 19:10:24 +0530

    printNodesWithoutSibling

---
+++ BinaryTrees/NodeWithOutSibling.java
@@ -0,0 +1,40 @@
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static void printNodesWithoutSibling(BinaryTreeNode<Integer> root) {
+		//Your code goes here
+        if(root==null)return;
+        else{
+             if(root.right==null&&root.left!=null){
+               System.out.print(root.left.data+" ");
+        
+           }
+           if(root.left==null&&root.right!=null){
+               System.out.print(root.right.data+" ");
+       
+           }
+           printNodesWithoutSibling(root.left);
+           printNodesWithoutSibling(root.right);
+           
+             
+        }
+	}
+
+}
