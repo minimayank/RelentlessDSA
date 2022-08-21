4752c945840f9e5f36f54c244920d7e1934e17f4
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-08-21 08:51:31 +0530

    Search In A BST

---
+++ BinarySearchTree/SearchInABST.java
@@ -0,0 +1,27 @@
+public class SearchInABST {
+
+	/*
+	 * Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T> right;
+	 * 
+	 * public BinaryTreeNode(T data) { this.data = data; } }
+	 */
+
+
+	public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+     if(root==null)return false;
+     if(root.data==k)return true;
+     else if(root.data>k){
+         return searchInBST(root.left,k);
+     }
+     else if(root.data<k){
+         return searchInBST(root.right,k);
+     }   
+     else{
+         return false;
+     }   
+      
+    
+    }
+}
