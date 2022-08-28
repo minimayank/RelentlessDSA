ec14d81e89180c60ec2a482693958115e7f46c7e
Mayank kumar <42250319+minimayank@users.noreply.github.com>
2022-08-28 09:02:07 +0530

    MirrorTree 2 java

---
+++ RelentlessRevision/BinaryTree/MirrorTree2.java
@@ -0,0 +1,32 @@
+/*************************************************************
+ 
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode 
+    {
+    
+        int data;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+
+        BinaryTreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+*************************************************************/
+
+public class Solution {
+	public static void mirrorTree(BinaryTreeNode node) {
+		// Write your code here.
+        if(node==null)return;
+     
+          mirrorTree(node.left);
+          mirrorTree(node.right);
+          BinaryTreeNode temp=node.left;
+          node.left=node.right;
+          node.right=temp;
+      }
+}
